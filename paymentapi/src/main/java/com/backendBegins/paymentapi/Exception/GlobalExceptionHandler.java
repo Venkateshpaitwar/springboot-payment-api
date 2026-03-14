06d8c734baf109d8c34ac21ce22c66740ec7e37f
Venkateshpaitwar <163770071+Venkateshpaitwar@users.noreply.github.com>
2026-03-14 15:49:35 +0530

    feat: implement structured API error response with ErrorResponse DTO

--- paymentapi/src/main/java/com/backendBegins/paymentapi/Exception/GlobalExceptionHandler.java
+++ paymentapi/src/main/java/com/backendBegins/paymentapi/Exception/GlobalExceptionHandler.java
@@ -1,15 +1,28 @@
 package com.backendBegins.paymentapi.Exception;
 
+import com.backendBegins.paymentapi.DTO.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(PaymentNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handlePaymentNotFound(PaymentNotFoundException ex) {
-        return ex.getMessage();
-    }
+    public ErrorResponse handlePaymentNotFound(
+            PaymentNotFoundException ex,
+            HttpServletRequest request
+    ) {
 
+        ErrorResponse error = new ErrorResponse();
+        error.setTimestamp(LocalDateTime.now());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(ex.getMessage());
+        error.setPath(request.getRequestURI());
+
+        return error;
+    }
 }
